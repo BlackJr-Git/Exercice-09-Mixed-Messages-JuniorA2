9e9decc2bd89c983f670311be558fbe1a84ae93b
BlackJr-Git <juniorassosa@gmail.com>
2023-07-28 15:31:06 +0100

    feat : displaying randome messages par running

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const greetings = ["Salut", "Bonjour", "Hello", "Coucou"];
-const question = ["ça va ?", "tu vas bien ?", "en forme?"];
-const wish = ["Passe une bonne journee", "Au revoir", "A demain"];
+const question = ["ça va", "tu vas bien", "en forme"];
+const wish = ["Passe une bonne journée", "Au revoir", "A demain"];
 
 let randomGreetting = Math.floor(Math.random() * greetings.length);
 let randomQuestion = Math.floor(Math.random() * question.length);
 let randomWish = Math.floor(Math.random() * wish.length);
 
-console.log(randomGreetting, randomQuestion, randomWish);
+console.log(
+  `${greetings[randomGreetting]} ! ${question[randomQuestion]} ? Super ! ${wish[randomWish]} `
+);
