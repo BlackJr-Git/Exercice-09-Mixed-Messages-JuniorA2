a8f7fa1805739b849c8089d42e082c37109e5dd1
BlackJr-Git <juniorassosa@gmail.com>
2023-07-28 16:34:40 +0100

    feat : remove comment

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const greetings = ["Salut", "Bonjour", "Hello", "Coucou"];
 const question = ["ça va", "tu vas bien", "en forme"];
 const wish = ["Passe une bonne journée", "Au revoir", "A demain"];
 
-// let randomGreetting = Math.floor(Math.random() * greetings.length);
-// let randomQuestion = Math.floor(Math.random() * question.length);
-// let randomWish = Math.floor(Math.random() * wish.length);
-
 function randomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
@@ -25,10 +21,3 @@ function randomMessage() {
 }
 
 randomMessage();
-
-// console.log(wish[randomIndex(wish)]);
-// console.log();
-
-// console.log(
-//   `${greetings[randomGreetting]} ! ${question[randomQuestion]} ? Super ! ${wish[randomWish]} `
-// );
