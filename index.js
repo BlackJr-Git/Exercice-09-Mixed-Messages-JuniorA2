c8dd33696be0434ddcc8f256c4c8fbfbce7e1069
BlackJr-Git <juniorassosa@gmail.com>
2023-07-28 15:56:41 +0100

    feat : fonction for random index of array and fonction for random message

--- index.js
+++ index.js
@@ -2,10 +2,27 @@ const greetings = ["Salut", "Bonjour", "Hello", "Coucou"];
 const question = ["ça va", "tu vas bien", "en forme"];
 const wish = ["Passe une bonne journée", "Au revoir", "A demain"];
 
-let randomGreetting = Math.floor(Math.random() * greetings.length);
-let randomQuestion = Math.floor(Math.random() * question.length);
-let randomWish = Math.floor(Math.random() * wish.length);
+// let randomGreetting = Math.floor(Math.random() * greetings.length);
+// let randomQuestion = Math.floor(Math.random() * question.length);
+// let randomWish = Math.floor(Math.random() * wish.length);
 
-console.log(
-  `${greetings[randomGreetting]} ! ${question[randomQuestion]} ? Super ! ${wish[randomWish]} `
-);
+function randomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+function randomMessage() {
+  const array = [greetings, question, wish];
+
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i][randomIndex(array[i])]);
+  }
+}
+
+randomMessage();
+
+// console.log(wish[randomIndex(wish)]);
+// console.log();
+
+// console.log(
+//   `${greetings[randomGreetting]} ! ${question[randomQuestion]} ? Super ! ${wish[randomWish]} `
+// );
